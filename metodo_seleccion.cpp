f6c99ca2d40fe6f2b32fadde8a7d3779fa4da098
Miriam <32524062+Miriam12@users.noreply.github.com>
2017-10-25 13:58:31 -0500

    Update metodo_seleccion.cpp

--- metodo_seleccion.cpp
+++ metodo_seleccion.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
-	int datos[5];
-	int temp;
+	int a;
+	int datos[a];
+	int temp,valor;
+	int alt=0; 
+	srand(time(NULL));
 	
-	for(int i=0;i<=4;i++){
-		
-			printf("Introduce el valor %d: \n", i+1);
-			scanf("%d",&datos[i]);
-	}
+	printf("Dame el Tamaño del arreglo:\n");
+	scanf("%i",&a);
+	
+	
 	printf("Los datos capturados fueron los siguientes: \n");
-				for (int i=0;i<=4;i++){
+				for (int i=0;i<a;i++){
+			
+			alt=rand()%121;
+		 	datos[i]=alt;
+		 	printf("%i ",datos[i]);
 					printf("Posicion %d: %d \n", i+1, datos[i]);
 				}
 				
@@ -27,8 +35,7 @@ int main(int argc, char** argv) {
 				printf("LOS NUMEROS ORDENADOS SON :\n");
 				for(int i=0; i<5; i++){
 					printf("%i ", datos[i]);
-				}
-				
+				}		
 		printf("\n");
     	system("pause");
 		system("cls");
